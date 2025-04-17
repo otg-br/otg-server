faed52c1df79eb2a2e4a6796a15a0d107c930512
Mateus Roberto <125809602+Mateuzkl@users.noreply.github.com>
2025-04-17 11:16:54 -0300

    lib clean

--- data/lib/libs.lua
+++ data/lib/libs.lua
@@ -13,7 +13,6 @@ dofile('data/lib/guild.lua')
 
 dofile('data/lib/modalwindow.lua')
 
-dofile('data/lib/lionrock.lua')
 
 -- Tables library
 dofile('data/lib/tables/table.lua')
@@ -21,17 +20,8 @@ dofile('data/lib/tables/table.lua')
 -- dofile('data/lib/dailyRewardLib.lua')
 
 dofile('data/lib/customlib/custom.lua')
-dofile('data/lib/customlib/holy-functions.lua')
 dofile('data/lib/customlib/guildLevel.lua')
-dofile('data/lib/customlib/events.lua')
 
--- events
-dofile('data/lib/events/battlefield_x4.lua')
-dofile('data/lib/events/battlefield_x2.lua')
-dofile('data/lib/events/lastman.lua')
-dofile('data/lib/events/zombie.lua')
-dofile('data/lib/events/castle.lua')
-dofile('data/lib/events/private_war.lua')
 
 -- Debugging helper function for Lua developers
 dofile('data/lib/debugging/dump.lua')
