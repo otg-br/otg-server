c6c8eeb5218e0a948266e8ab9f8c06de07786485
Mateuzkl <mateus.roberto@educacao.mg.gov.br>
2025-06-30 18:58:45 -0300

    Add custom action scripts under actions/others

---
+++ data/scripts/actions/others/vipScroll.lua
@@ -0,0 +1,11 @@
+-- VIP Days Action Revscript
+local vipDaysAction = Action()
+function vipDaysAction.onUse(player, item, fromPosition, target, toPosition, isHotkey)
+    local days = 30  -- Fixed 30 days VIP
+    item:remove(1)
+    player:addVipDays(days)
+    player:sendTextMessage(MESSAGE_INFO_DESCR, string.format("You won %d days VIP, now you have %d days VIP.", days, math.max(0, math.ceil((player:getVipDays() - os.time()) / 86400))))
+    return true
+end
+vipDaysAction:id(16101)
+vipDaysAction:register()
